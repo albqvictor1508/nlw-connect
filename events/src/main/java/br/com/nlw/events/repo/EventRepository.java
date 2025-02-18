9ae041b291ca6fcc3014e49e50b7828c5c55a95b
albqvictor1508 <albq.victor@gmail.com>
2025-02-18 11:49:35 -0300

    chore: create event repository

---
+++ events/src/main/java/br/com/nlw/events/repo/EventRepository.java
@@ -0,0 +1,7 @@
+package br.com.nlw.events.repo;
+
+import br.com.nlw.events.model.EventModel;
+import org.springframework.data.repository.CrudRepository;
+
+public interface EventRepository extends CrudRepository<EventModel, Integer> {
+}
