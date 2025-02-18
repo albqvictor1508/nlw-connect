c10cebcc90493c2db3eba5fc0d3d6af2fc1d907a
albqvictor1508 <albq.victor@gmail.com>
2025-02-18 16:29:05 -0300

    chore: add user repository

--- events/src/main/java/br/com/nlw/events/repo/UserRepository.java
+++ events/src/main/java/br/com/nlw/events/repo/UserRepository.java
@@ -4,4 +4,5 @@ import br.com.nlw.events.model.UserModel;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<UserModel, Integer> {
+    public UserModel findByEmail(String email);
 }
