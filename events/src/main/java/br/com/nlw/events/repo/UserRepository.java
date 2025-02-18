77fb29338317b9d6332457aa4b84263345e8b8bf
albqvictor1508 <albq.victor@gmail.com>
2025-02-18 15:40:51 -0300

    chore: add user repository

---
+++ events/src/main/java/br/com/nlw/events/repo/UserRepository.java
@@ -0,0 +1,7 @@
+package br.com.nlw.events.repo;
+
+import br.com.nlw.events.model.UserModel;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<UserModel, Integer> {
+}
