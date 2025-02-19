5e25e73cbf7c46e24abc303529a11d29bc32a030
albqvictor1508 <albq.victor@gmail.com>
2025-02-18 23:49:57 -0300

    chore: add subscription model and volume on mysql container

---
+++ events/src/main/java/br/com/nlw/events/model/SubscriptionModel.java
@@ -0,0 +1,48 @@
+package br.com.nlw.events.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tbl_subscription")
+public class SubscriptionModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "subscription_number")
+    private Integer subscriptionNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private EventModel event;
+
+    @ManyToOne
+    @JoinColumn(name = "subscribed_user_id")
+    private UserModel subscriber;
+
+    @ManyToOne
+    @JoinColumn(name = "indication_user_id", nullable = true) //posso entrar sem ser indicado
+    private UserModel indication;
+
+    public Integer getSubscriptionNumber() {
+        return subscriptionNumber;
+    }
+
+    public void setSubscriptionNumber(Integer subscriptionNumber) {
+        this.subscriptionNumber = subscriptionNumber;
+    }
+
+    public EventModel getEvent() {
+        return event;
+    }
+
+    public void setEvent(EventModel event) {
+        this.event = event;
+    }
+
+    public UserModel getSubscriber() {
+        return subscriber;
+    }
+
+    public void setSubscriber(UserModel subscriber) {
+        this.subscriber = subscriber;
+    }
+}
