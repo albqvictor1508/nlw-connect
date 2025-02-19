fa4265766345309dcfdd03e9c757ea8310dbb3bf
albqvictor1508 <albq.victor@gmail.com>
2025-02-19 12:05:20 -0300

    feat: indications ranking feature completed

--- events/src/main/java/br/com/nlw/events/controller/SubscriptionController.java
+++ events/src/main/java/br/com/nlw/events/controller/SubscriptionController.java
@@ -9,10 +9,7 @@ import br.com.nlw.events.model.UserModel;
 import br.com.nlw.events.service.SubscriptionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class SubscriptionController {
@@ -36,5 +33,14 @@ public class SubscriptionController {
             return ResponseEntity.status(404).body(new ErrorMessage(ex.getMessage()));
         }
         return ResponseEntity.badRequest().build();
+    }
+
+    @GetMapping("subscription/{prettyName}/ranking")
+    public ResponseEntity<?> generateRankingByEvent(@PathVariable String prettyName) {
+        try {
+            return ResponseEntity.ok((service.getCompleteRanking(prettyName)));
+        } catch (EventNotFoundException e) {
+            return ResponseEntity.status(404).body(new ErrorMessage(e.getMessage()));
+        }
     }
 }
--- events/src/main/java/br/com/nlw/events/repo/SubscriptionRepository.java
+++ events/src/main/java/br/com/nlw/events/repo/SubscriptionRepository.java
@@ -13,12 +13,12 @@ import java.util.List;
 public interface SubscriptionRepository extends CrudRepository<SubscriptionModel, Integer> {
      public SubscriptionModel findByEventAndSubscriber(EventModel evt, UserModel subscriber);
 
-     @Query(value = "SELECT COUNT(subscription_number) AS quantity, indication_user_id, user_name"
-             + "FROM tbl_subscription INNER JOIN tbl_user"
-             + "ON tbl_subscription.indication_user_id = tbl_user.user_id"
-             + "WHERE indication_user_id IS NOT NULL"
-             + "AND event_id = :eventId"
-             + "GROUP BY indication_user_id"
+     @Query(value = "SELECT COUNT(subscription_number) AS quantity, indication_user_id, user_name "
+             + "FROM tbl_subscription INNER JOIN tbl_user "
+             + "ON tbl_subscription.indication_user_id = tbl_user.user_id "
+             + "WHERE indication_user_id IS NOT NULL "
+             + "AND event_id = :eventId "
+             + "GROUP BY indication_user_id "
              + "ORDER BY quantity desc", nativeQuery = true)
      public List<SubscriptionRankingItem> generateRanking(@Param("eventId") Integer eventId);
 }
